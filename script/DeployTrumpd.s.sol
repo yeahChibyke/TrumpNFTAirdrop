5a4acfa0671fe3b1b5fede720bb43f38e7420996
yeahChibyke <chibykepride@gmail.com>
2024-08-26 11:52:30 +0100

    wrote deploy script. renamed svg file

---
+++ script/DeployTrumpd.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: mit
+pragma solidity ^0.8.26;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {Trumpd} from "../src/Trumpd.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployTrumpd is Script {
+    function run() external returns (Trumpd) {
+        string memory trumpdSvg = vm.readFile("./img/trumpd.svg");
+
+        vm.startBroadcast();
+        Trumpd trumpd = new Trumpd(svgToImageURI(trumpdSvg));
+        vm.stopBroadcast();
+
+        return trumpd;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
